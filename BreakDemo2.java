f2564dd3c6ee7a5a6b35461f7f81404729d52703
dougan8787 <36374185+dougan8787@users.noreply.github.com>
2019-12-27 16:52:54 +0800

    Add files via upload

--- BreakDemo2.java
+++ BreakDemo2.java
@@ -6,7 +6,7 @@ public class BreakDemo2 {
 		// TODO Auto-generated method stub
 		outer:for(int i=0;i<=5;i++) {
 			for(int j=0;j<=3;j++) {
-				System.out.println("i value:"+i+"j value:"+j+"\n");
+				System.out.println("i value:"+i+"j value:"+j+"");
 				if(j==1) {
 					
 					break outer;
---
+++ ContinueDemo.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class ContinueDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 1;i<=10;i++) {
+			if(i==5) {
+				System.out.println("get value continue");
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
